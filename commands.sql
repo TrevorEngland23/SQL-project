d901b9c6dfddd3fa7ec89478267620f77c0dd4a5
TrevorEngland23 <trevor.england.523@gmail.com>
2024-12-06 04:53:10 -0800

    procedure for mnaul refresh

--- commands.sql
+++ commands.sql
@@ -1,29 +1,6 @@
 -- Business Question: "During the summer months (June 1 - August 31) for the available data, what were the top three genres that customers rented?"
 
--- Summary Table Query
-SELECT cat.name AS genre, COUNT(r.rental_id) AS rental_count
-FROM rental AS r
-INNER JOIN inventory AS i ON r.inventory_id = i.inventory_id
-INNER JOIN film AS f ON i.film_id = f.film_id
-INNER JOIN film_category AS fc ON f.film_id = fc.film_id
-INNER JOIN category AS cat ON fc.category_id = cat.category_id
-WHERE r.rental_date BETWEEN '2005-06-01' AND '2005-09-01'
-GROUP BY cat.name
-ORDER BY rental_count DESC
-LIMIT 3;
 
--- Detail Table Query
-SELECT r.rental_date, cust.first_name, cust.last_name, f.title AS movie, cat.name AS genre
-FROM rental AS r
-INNER JOIN inventory AS i ON r.inventory_id = i.inventory_id
-INNER JOIN film AS f ON i.film_id = f.film_id
-INNER JOIN film_category AS fc ON f.film_id = fc.film_id
-INNER JOIN category AS cat ON fc.category_id = cat.category_id
-INNER JOIN customer as cust ON r.customer_id = cust.customer_id
-WHERE r.rental_date BETWEEN '2005-06-01' AND '2005-09-01'
-AND cat.name IN ('Sports', 'Animation', 'Sci-Fi') 
--- here we may be able to create a function that populates the data based on the the summary table
-ORDER BY r.rental_date DESC;
 
 -- User defined function on first and last name columns.
 CREATE OR REPLACE FUNCTION get_customer_name(first_name VARCHAR(45), last_name VARCHAR(45))
@@ -45,22 +22,26 @@ $$ LANGUAGE plpgsql
 CREATE OR REPLACE FUNCTION get_top_three_genres()
 RETURNS VARCHAR[] AS $$
 DECLARE
-	top_three_genres_list VARCHAR[];
+    top_three_genres_list VARCHAR[];
 BEGIN
-	SELECT array_agg(cat.name)
-	INTO top_three_genres_list
-	FROM (
-		SELECT rs.genre, rs.total_rentals
-		FROM rental_summary AS rs
-		ORDER BY rs.total_rentals DESC
-		LIMIT 3
-	) AS top_three_genres
-	INNER JOIN category AS cat ON top_three_genres.genre = cat.name;
+    SELECT array_agg(genre)
+    INTO top_three_genres_list
+    FROM (
+        SELECT cat.name AS genre, COUNT(r.rental_id) AS rental_count
+        FROM rental AS r
+        INNER JOIN inventory AS i ON r.inventory_id = i.inventory_id
+        INNER JOIN film AS f ON i.film_id = f.film_id
+        INNER JOIN film_category AS fc ON f.film_id = fc.film_id
+        INNER JOIN category AS cat ON fc.category_id = cat.category_id
+        WHERE r.rental_date BETWEEN '2005-06-01' AND '2005-09-01'
+        GROUP BY cat.name
+        ORDER BY rental_count DESC
+        LIMIT 3
+    ) AS top_genres;
 
-	RETURN top_three_genres_list;
+    RETURN top_three_genres_list;
 END;
 $$ LANGUAGE plpgsql;
-
 
 -- Create summary table
 CREATE TABLE rental_summary (
@@ -158,24 +139,22 @@ FOR EACH STATEMENT
 EXECUTE PROCEDURE update_summary_on_delete();
 
 -- Create procedure to refresh table data
-
 CREATE OR REPLACE PROCEDURE refresh_summary_and_details()
 AS $$
 BEGIN
 	DELETE FROM rental_details;
 
 	INSERT INTO rental_details
-	SELECT r.rental_id, r.rental_date, cust.customer_id, 		get_customer_name(cust.first_name, cust.last_name) AS 		customer_name, f.title AS movie_title, cat.name AS 		genre, f.film_id, cat.category_id
-	FROM rental AS r
-	INNER JOIN inventory AS i ON r.inventory_id = 				i.inventory_id
-	INNER JOIN film AS f ON i.film_id = f.film_id
-	INNER JOIN film_category AS fc ON f.film_id = fc.film_id
-	INNER JOIN category AS cat ON fc.category_id = 			cat.category_id
-	INNER JOIN customer as cust ON r.customer_id = 			cust.customer_id
-	WHERE r.rental_date BETWEEN '2005-06-01' AND '2005-09-01'
-	AND cat.name IN (SELECT unnest(get_top_three_genres()))
-	ORDER BY r.rental_date DESC;
-
+SELECT r.rental_id, r.rental_date, cust.customer_id, get_customer_name(cust.first_name, cust.last_name) AS customer_name, f.title AS movie_title, cat.name AS genre, f.film_id, cat.category_id
+FROM rental AS r
+INNER JOIN inventory AS i ON r.inventory_id = i.inventory_id
+INNER JOIN film AS f ON i.film_id = f.film_id
+INNER JOIN film_category AS fc ON f.film_id = fc.film_id
+INNER JOIN category AS cat ON fc.category_id = cat.category_id
+INNER JOIN customer as cust ON r.customer_id = cust.customer_id
+WHERE r.rental_date BETWEEN '2005-06-01' AND '2005-09-01'
+AND cat.name IN (SELECT unnest(get_top_three_genres()))
+ORDER BY r.rental_date DESC;
 	RETURN;
 END;
 $$ LANGUAGE plpgsql;
@@ -184,30 +163,72 @@ $$ LANGUAGE plpgsql;
 CALL refresh_summary_and_details();
 
 -- Troubleshooting Queries ---------------------------------------------------------
-INSERT INTO rental_details (rental_date, customer_id, customer_name, movie_title, genre, film_id, category_id)
+INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id)
 VALUES (
-    '2005-07-01 14:30:00',
-    131,                    -- customer_id (should exist in the customer table)
-    'Monica Hicks',             -- customer_name
-    'River Outlaw',         -- movie_title
-    'Sports',               -- genre
-    733,                    -- film_id (should exist in the film table)
-    15                      -- category_id (should exist in the category table)
+    '2005-06-23',
+    130,
+    85,
+    '2005-07-01',
+    2 
 );
 
-DELETE FROM rental_details
-WHERE rental_date = '2005-07-01 14:30:00'
-  AND customer_id = 131
-  AND movie_title = 'River Outlaw';
+SELECT DISTINCT i.inventory_id
+FROM inventory AS i
+INNER JOIN film AS f ON i.film_id = f.film_id
+INNER JOIN film_category AS fc ON f.film_id = fc.film_id
+INNER JOIN category AS cat ON fc.category_id = cat.category_id
+WHERE cat.name IN (
+    SELECT genre
+    FROM rental_summary
+)
+ORDER BY i.inventory_id;
 
-SELECT * 
-FROM rental_details
-WHERE customer_id = 131;
 
+SELECT * FROM inventory
+
+
+
+SELECT * FROM rental
+WHERE customer_id = 85 AND inventory_id = 152;
+
+DELETE FROM rental
+WHERE customer_id = 85 AND inventory_id = 152;
 
 SELECT * FROM rental_summary
 SELECT * FROM rental_details
+
+SELECT * FROM rental_details
+WHERE customer_id = 85 
+
 SELECT COUNT(rental_id) FROM rental_details
 DELETE FROM rental_details;
 DROP TABLE rental_details;
 DROP TABLE rental_summary
+
+-- QUERIES FROM THE BEGINNING
+
+-- Summary Table Query
+SELECT cat.name AS genre, COUNT(r.rental_id) AS rental_count
+FROM rental AS r
+INNER JOIN inventory AS i ON r.inventory_id = i.inventory_id
+INNER JOIN film AS f ON i.film_id = f.film_id
+INNER JOIN film_category AS fc ON f.film_id = fc.film_id
+INNER JOIN category AS cat ON fc.category_id = cat.category_id
+WHERE r.rental_date BETWEEN '2005-06-01' AND '2005-09-01'
+GROUP BY cat.name
+ORDER BY rental_count DESC
+LIMIT 3;
+
+-- Detail Table Query
+SELECT r.rental_date, cust.first_name, cust.last_name, f.title AS movie, cat.name AS genre
+FROM rental AS r
+INNER JOIN inventory AS i ON r.inventory_id = i.inventory_id
+INNER JOIN film AS f ON i.film_id = f.film_id
+INNER JOIN film_category AS fc ON f.film_id = fc.film_id
+INNER JOIN category AS cat ON fc.category_id = cat.category_id
+INNER JOIN customer as cust ON r.customer_id = cust.customer_id
+WHERE r.rental_date BETWEEN '2005-06-01' AND '2005-09-01'
+AND cat.name IN ('Sports', 'Animation', 'Sci-Fi') 
+-- here we may be able to create a function that populates the data based on the the summary table
+ORDER BY r.rental_date DESC;
+
